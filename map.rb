340ceba8ea982442902a1c205129036438b4d435
kali2 <kali2@kali.kali>
2024-05-20 18:54:58 +0300

    map

---
+++ map.rb
@@ -0,0 +1,41 @@
+
+require 'gosu'
+require_relative 'log'
+
+
+
+class Map
+
+def initailize(window)
+		@window = window
+
+	end
+
+
+
+	def draw
+	end
+
+
+
+
+	def update
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### map.rb
+
+
