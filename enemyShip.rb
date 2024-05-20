1b60493f5936128371bca62439baaba419113ea3
kali2 <kali2@kali.kali>
2024-05-20 14:14:54 +0300

    lopy

--- enemyShip.rb
+++ enemyShip.rb
@@ -6,13 +6,32 @@ class EnemyShip
 	def initialize(window)
 		@window=window
 
+		@x,@y=100,100
+		@width = 50
+		@height = 30
 
+		@life = 50
 	end
 
 	def draw
+		draw_ship(@x,@y,@width,@height) if @life > 0
+	end
+
+	def draw_ship(x,y,w,h)
+		@window.draw_quad(	x, 		y, 	 Gosu::Color::BLUE,
+        					x+w, 	y, 	 Gosu::Color::BLUE,
+        					x+w, 	y+h, Gosu::Color::RED,
+    						x,		y+h, Gosu::Color::RED)	
 	end
 
 	def update
+				
+	end
+
+	def hit(x,y)
+		if x>=@x&&x<@x+@width&&y<@y+@height
+			@life -= 5
+		end
 	end
 
 end
