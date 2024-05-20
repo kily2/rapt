0f966f35eb6bc6ebeb197d04bfe10debfa204d31
kali2 <kali2@kali.kali>
2024-05-20 15:45:14 +0300

    beauty and the fist

--- playerShip.rb
+++ playerShip.rb
@@ -21,52 +21,63 @@ class PlayerShip
 		@shoot_interval = 100
 	end
 
+
+
 	def draw
-		draw_ship(@x,@y,@width,@height)
+		draw_ship
 		@balls.each(&:draw)
 	end
 
+
+
 	def update	
-		move_ship(@window.mouse_x,@window.mouse_y)
-
-		current_time = Gosu.milliseconds
-		elapsed_time = (current_time - @last_shoot_time)
-
-		
-		if @start_shoot && elapsed_time > @shoot_interval
-			shoot
-			@last_shoot_time = current_time
-		end
-
+		move_ship
+		shooting
 		@balls.each(&:update)
 	end
 
+
 	##########################################
 
-	
-	def draw_ship(x,y,w,h)
-	    @window.draw_triangle(
-      		x, y, Gosu::Color::GREEN,
-      		x-w/2, y+h, Gosu::Color::GREEN,
-      		x+w/2, y+h, Gosu::Color::GREEN
-    	)
 
+
+	def shooting
+		current_time = Gosu.milliseconds
+		elapsed_time = (current_time - @last_shoot_time)
+		if @start_shoot && elapsed_time > @shoot_interval
+			one_shoot
+			@last_shoot_time = current_time
+		end
 	end
 
 
-	def move_ship(x,y)		
-		@x,@y=x,y
+
+	def one_shoot
+		@balls[@ball_n].shoot(@x,@y)
+		@ball_n+=1
+		@ball_n%=@balls.size
+	end
+
+
+
+	def draw_ship
+	    @window.draw_triangle(
+      		@x, 			@y, 		Gosu::Color::GREEN,
+      		@x-@width/2,	@y+@height, Gosu::Color::GREEN,
+      		@x+@width/2,	@y+@height, Gosu::Color::GREEN)
+	end
+
+
+
+	def move_ship
+		@x,@y= @window.mouse_x.dup,	@window.mouse_y.dup
 		@x=@width/2					unless @x>0
 		@x=@window.width-@width/2	unless @x<@window.width
 		@y=0						unless @y>0
 		@y=@window.height-@height 	unless @y<@window.height
 	end
 
-	def shoot
-		@balls[@ball_n].shoot(@x,@y)
-		@ball_n+=1
-		@ball_n%=@balls.size
-	end
+
 
 	def button_down(id)
 		case id
@@ -77,6 +88,8 @@ class PlayerShip
 			#change weapon
 		end
 	end
+
+
 
 	def button_up(id)
 		case id
